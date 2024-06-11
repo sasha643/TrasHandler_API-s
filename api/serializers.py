213adfacf9fca44fbfcd7826ea8fd29f5fe6b504
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2024-06-11 22:29:46 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -12,6 +12,10 @@ class VendorAuthSerializer(serializers.ModelSerializer):
     class Meta:
         model = VendorAuth
         fields = '__all__'
+        
+
+class VendorSigninSerializer(serializers.Serializer):
+    mobile_no = serializers.CharField(max_length=15)
 
 
 class PhotoUploadSerializer(serializers.ModelSerializer):
@@ -21,8 +25,32 @@ class PhotoUploadSerializer(serializers.ModelSerializer):
         model = PhotoUpload
         fields = '__all__'
 
-class VendorLocationSerializer(serializers.ModelSerializer):
+class CustomerSigninSerializer(serializers.Serializer):
+    mobile_no = serializers.CharField(max_length=15)
+
+
+class CustomerLocationSerializer(serializers.ModelSerializer):
+    customer_id = serializers.IntegerField(write_only=True)
+
     class Meta:
-        model = VendorLocation
-        read_only_fields = ['vendor'] 
-        fields = ['vendor', 'latitude', 'longitude']
+        model = CustomerLocation
+        fields = ['customer_id', 'latitude', 'longitude']
+
+    def create(self, validated_data):
+        customer_id = validated_data.pop('customer_id')
+        customer = CustomerAuth.objects.get(id=customer_id)
+        location = CustomerLocation.objects.create(customer=customer, **validated_data)
+        return location
+
+class VendorCompleteProfileSerializer(serializers.ModelSerializer):
+    vendor_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = VendorCompleteProfile
+        fields = ['vendor_id', 'gstin_number', 'business_name', 'pan_card', 'business_photos']
+
+    def create(self, validated_data):
+        vendor_id = validated_data.pop('vendor_id')
+        vendor = VendorAuth.objects.get(id=vendor_id)
+        profile = VendorCompleteProfile.objects.create(vendor=vendor, **validated_data)
+        return profile
