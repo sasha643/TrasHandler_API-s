2416471dd9a7a8273ffbcffdf919be7f29febe9f
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2024-06-11 22:31:03 +0530

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -12,10 +12,16 @@ class VendorAuthModelAdmin(admin.ModelAdmin):
 class PhotoUploadModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'photo', 'description', 'landmark', 'time_slot']
 
-class VendorLocationModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'vendor', 'latitude', 'longitude']
+class CustomerLocationModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'latitude', 'longitude']
+    search_fields = ['customer__name', 'customer__mobile_no']
+
+class VendorCompleteProfileModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'vendor', 'gstin_number', 'business_name', 'pan_card', 'business_photos']
+    search_fields = ['vendor__name', 'vendor__mobile_no']
 
 admin.site.register(CustomerAuth, CustomerAuthModelAdmin)
 admin.site.register(VendorAuth, VendorAuthModelAdmin)
 admin.site.register(PhotoUpload, PhotoUploadModelAdmin)
-admin.site.register(VendorLocation, VendorLocationModelAdmin)
+admin.site.register(CustomerLocation, CustomerLocationModelAdmin)
+admin.site.register(VendorCompleteProfile, VendorCompleteProfileModelAdmin)
