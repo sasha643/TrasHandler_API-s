c307b66e485b8b1619d48ec68d72837fbb0d95d6
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2024-06-12 14:48:20 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -186,3 +186,42 @@ class VendorCompleteProfileViewSet(viewsets.GenericViewSet, mixins.CreateModelMi
 
     def perform_create(self, serializer):
         serializer.save()
+
+
+class VendorLocationViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    queryset = VendorLocation.objects.all()
+    serializer_class = VendorLocationSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        vendor_id = request.data.get('vendor_id')
+        if not vendor_id:
+            return Response({"error": "vendor ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            vendor = VendorAuth.objects.get(id=vendor_id)
+        except VendorAuth.DoesNotExist:
+            return Response({"error": "vendor profile not found for the provided ID"}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            vendor_location = VendorLocation.objects.get(vendor=vendor)
+            # If an entry exists, update it
+            serializer = self.get_serializer(vendor_location, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+        except VendorLocation.DoesNotExist:
+            # If no entry exists, create a new one
+            data = request.data.copy()
+            data['vendor_id'] = vendor.id
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_create(self, serializer):
+        serializer.save()
