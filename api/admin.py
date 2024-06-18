5b1816948fe3932ef7a635139b2ca989157acdb5
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2024-06-18 15:56:44 +0530

    added for pickup request

--- api/admin.py
+++ api/admin.py
@@ -24,9 +24,14 @@ class VendorLocationModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'vendor', 'latitude', 'longitude']
     search_fields = ['vendor__name', 'vendor__mobile_no']
 
+class PickupRequestModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'latitude', 'longitude', 'vendor', 'status']
+    search_fields = ['customer__name', 'customer__mobile_no', 'vendor__name', 'vendor__mobile_no', 'status']
+
 admin.site.register(CustomerAuth, CustomerAuthModelAdmin)
 admin.site.register(VendorAuth, VendorAuthModelAdmin)
 admin.site.register(PhotoUpload, PhotoUploadModelAdmin)
 admin.site.register(CustomerLocation, CustomerLocationModelAdmin)
 admin.site.register(VendorCompleteProfile, VendorCompleteProfileModelAdmin)
 admin.site.register(VendorLocation, VendorLocationModelAdmin)
+admin.site.register(PickupRequest, PickupRequestModelAdmin)
