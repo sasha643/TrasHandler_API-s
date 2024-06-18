0c89a9e91e84d7b83e9b880475b20b4064155257
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2024-06-18 15:49:56 +0530

    added model for pickup request

--- api/models.py
+++ api/models.py
@@ -65,3 +65,13 @@ class VendorLocation(models.Model):
 
     def __str__(self):
         return f'{self.vendor.name} - {self.latitude}, {self.longitude}'
+
+class PickupRequest(models.Model):
+    customer = models.ForeignKey(CustomerAuth, on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    vendor = models.ForeignKey(VendorAuth, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=50, default="Pending")
+
+    def __str__(self):
+        return f'Request by {self.customer.name} - {self.status}'
