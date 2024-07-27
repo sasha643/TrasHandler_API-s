edf49f63eabcc92f2e0c4258c9d860a1fefbe2cb
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-07-27 23:01:20 +0530

    Update settings.py

--- trashapi/settings.py
+++ trashapi/settings.py
@@ -164,7 +164,7 @@ CORS_ORIGIN_ALLOW_ALL = True  # For development only. In production, specify all
 #     'http://localhost:8000',
 # ]
 
-ALLOWED_HOSTS = ['trashandler-api-s-1.onrender.com', 'trashandler-api-s-2.onrender.com']
+ALLOWED_HOSTS = ['trashandler-api-s-n9nw.onrender.com']
 
 CORS_ALLOW_METHODS = [
     'GET',
