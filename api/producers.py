ba895b23950ed3325249fa6e44d36b771e83c4f5
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-09-07 19:12:45 +0530

    Update producers.py

--- api/producers.py
+++ api/producers.py
@@ -1,4 +1,4 @@
-from confluent_kafka import KafkaProducer
+from kafka import KafkaProducer
 import json
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 def send_location_update(action, data, vendor_id):
