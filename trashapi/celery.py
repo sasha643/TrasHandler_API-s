44417f07e63b376e71ef768493594ade6cf34656
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-09-07 19:04:33 +0530

    Update celery.py

--- trashapi/celery.py
+++ trashapi/celery.py
@@ -6,7 +6,7 @@ from celery.schedules import crontab, schedule
 
 
 # set the default Django settings module for the 'celery' program.
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trashapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trashapi.settings')
 
 
 app = Celery('trashapi')
