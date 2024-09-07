50ee25b35c0395b7581d61e0750a1984fc36902c
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-09-08 01:09:13 +0530

    Update settings.py

--- trashapi/settings.py
+++ trashapi/settings.py
@@ -258,7 +258,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('13.232.64.63', 6379)],
         },
     },
 }
