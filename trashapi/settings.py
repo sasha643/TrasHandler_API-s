1aed92f116f725f4d19bc078bbcfcfdcf4224f36
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-09-07 15:14:34 +0530

    Update settings.py

--- trashapi/settings.py
+++ trashapi/settings.py
@@ -166,7 +166,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Abdul',
         'USER': 'postgres',
-        'PASSWORD': 'Dhruv@251103',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',  
         'PORT': '5432',        
     }
