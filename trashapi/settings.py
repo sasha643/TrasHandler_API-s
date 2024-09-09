f42c95123056e0b5695407f99495026a97a73baf
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-09-09 19:31:30 +0530

    Update settings.py

--- trashapi/settings.py
+++ trashapi/settings.py
@@ -164,10 +164,10 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TrasHandler',                      # Replace with your database name
+        'NAME': 'Trashandler',                      # Replace with your database name
         'USER': 'postgres',                  # Replace with your database username
         'PASSWORD': 'postgres',         # Replace with your database password
-        'HOST': '13.232.64.63',             # Replace with your public IP address of local machine
+        'HOST': '3.108.52.92',             # Replace with your public IP address of local machine
         'PORT': '5432',                      # Default PostgreSQL port
     }
 }
