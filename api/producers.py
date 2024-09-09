e839709d4dd91cb37aa01bf67ee920f38b1c5668
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-09-09 18:56:03 +0530

    Update producers.py

--- api/producers.py
+++ api/producers.py
@@ -3,7 +3,7 @@ import json
 
 # Kafka Producer configuration for Confluent Kafka
 conf = {
-    'bootstrap.servers': '13.232.64.63:9092',  # Kafka broker address
+    'bootstrap.servers': '3.108.52.92:9092',  # Kafka broker address
     'client.id': 'vendor-location-producer',  # Client identifier for the producer
     'acks': 'all',  # Wait for acknowledgment from all replicas
 }
