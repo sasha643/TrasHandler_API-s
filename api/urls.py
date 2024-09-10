fd912ecb252de3e9f0bc2e6262a89d2bd9480f9b
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2024-09-10 20:21:31 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -45,9 +45,11 @@ doc_path = os.path.join(os.path.dirname(__file__), 'docs/_build/html')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
-    re_path(r'^api/docs/$', serve, {'path': 'index.html', 'document_root': doc_path}),    
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
+    re_path(r'^documentation/$', serve, {'path': 'index.html', 'document_root': doc_path}),
+    
     # Serve other Sphinx documentation files
-    re_path(r'^api/docs/(?P<path>.*)$', serve, {'document_root': doc_path}),
+    re_path(r'^documentation/(?P<path>.*)$', serve, {'document_root': doc_path}),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/customer/register/', CustomerAuthRegisterView.as_view(), name='customer-register'),
     path('api/auth/vendor/register/', VendorAuthRegisterView.as_view(), name='vendor-register'),
