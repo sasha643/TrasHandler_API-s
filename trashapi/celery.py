c1aec7d47cd4f8c28900f5ec0108e41f58ceef4b
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2024-09-20 23:51:27 +0530

    Update celery.py

--- trashapi/celery.py
+++ trashapi/celery.py
@@ -1,25 +1,22 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from trashapi import settings
-from celery.schedules import crontab, schedule
+from celery.schedules import crontab
 
-
-# set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trashapi.settings')
-
 
 app = Celery('trashapi')
 
+# Load settings
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# Celery Beat Schedule
 app.conf.beat_schedule = {
-
-    'refresh-tokens-every-10-minutes': {
+    'refresh-tokens-every-5-minutes': {
         'task': 'api.tasks.refresh_tokens',
-        'schedule': crontab(minute='*/10'),  # Run every minutes
+        'schedule': crontab(minute='*/5'),  # Run every 5 minutes
     },
-
 }
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# Discover tasks in Django apps
 app.autodiscover_tasks()
