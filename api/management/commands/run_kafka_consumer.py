1ba971235a98fb1f4f9a0fcef37929aef32165b0
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2025-03-06 00:16:34 +0530

    Update run_kafka_consumer.py

--- api/management/commands/run_kafka_consumer.py
+++ api/management/commands/run_kafka_consumer.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Configuration for Confluent Kafka Consumer
         conf = {
-            'bootstrap.servers': '13.201.99.143:9092',  # Kafka broker address
+            'bootstrap.servers': '3.110.42.80:9092',  # Kafka broker address
             'group.id': 'vendor-location-group',  # Required group ID for the consumer
             'client.id': 'vendor-location-consumer',  # Client identifier for the consumer
             'auto.offset.reset': 'earliest',  # Start reading at the earliest available message
